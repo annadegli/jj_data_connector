df53f0ee0125ac6d788dc3fce559eafd6d2277ff
jiejenn@learndataanalysis.org <jiejenn@learndataanalysis.org>
2022-08-05 12:22:10 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,6 @@ setup(
     keywords=['data connector', 'google analtics 4', 'GA4', 'Salesforce', 'sfdc', 'Google Search Console'],
     python_requires='>=3.6',
     install_requires=['google-api-python-client>=2.51.0', 'google-auth-httplib2>=0.1.0', 'google-auth-oauthlib>=0.5.2', 'requests>=2.28.0'],
-    packages=find_packages(),
-    # packages=['jj_data_connector'],
+    packages=['jj_data_connector', 'jj_data_connector.ga4', 'jj_data_connector.gsc', 'jj_data_connector.sfdc'],
     license='MIT',
 )   
